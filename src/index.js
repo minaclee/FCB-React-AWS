ea0abd72ec749d6b062f3df68d23032772baa5f4
Normin Paulo Gorospe <norminpg101@gmail.com>
2022-01-20 16:00:42 +0800

    Apply external styles to a component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
 
 //My First Component
-function HelloWorld(){
-	return <h1>Hello, World!</h1>;
+function App() {
+  return (
+    <div className ="app">
+      <header className="app-header">
+        <h1>My Amazing ToDo-List App</h1>
+        <p>The most simple and amazing todo-list React app.</p>
+      </header>
+    </div>
+  );
 }
 
 
-ReactDOM.render(<HelloWorld />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"));
